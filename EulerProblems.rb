7d52d8b723920a1348736a8dd96e3e7ddcae270d
Kevin Biddle <kdbiddle08@gmail.com>
2015-12-27 19:26:42 -0500

    Completed Multiples of 3 and 5 problem in Ruby

---
+++ EulerProblems.rb
@@ -0,0 +1,18 @@
+def problem_one
+	i = 0
+	max = 1000
+	total = 0
+	while i<max do
+		if (i%3==0 or i%5==0)
+			total += i
+		end
+		i += 1
+	puts total
+	end
+end
+
+problem_one
+
+def problem_two
+	
+end
